9bea4e77f27c2fd1b298689d0f11d5b2150d746a
TM-ARUN <tmarunajayan@gmail.com>
2025-07-13 03:09:35 +0530

    Created url routes for api endpoints

---
+++ product_review_system/app_reviews/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('register/',RegisterView.as_view(), name='register'),
+    path('login/',CustomAuthToken.as_view(),name='login'),
+    path('logout/',LogoutView.as_view(),name='logout'),
+    path('products/',ProductListCreateView.as_view(), name='product-list'),
+    path('products/<int:pk>',ProductRetrieveUpdateDestroyView.as_view(), name='product-detail'),
+    path('products/<int:product_id>/reviews/', ReviewListCreateView.as_view(), name='review-list'),
+    
+]
