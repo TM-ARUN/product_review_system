ac0644da4850d1bce2825e17ce8a87c8bbf123cf
TM-ARUN <tmarunajayan@gmail.com>
2025-07-13 01:26:50 +0530

    implement views for user registration,login and logout

--- product_review_system/app_reviews/views.py
+++ product_review_system/app_reviews/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from rest_framework import generics,permissions,status
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from django.contrib.auth import logout
+from .models import *
+from .serializers import *
 
 # Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+    
+    def perform_create(self, serializer):
+        user = serializer.save()
+        if user:
+            Token.objects.create(user = user)
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data = request.data,context = {'request':request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token,created = Token.objects.get_or_create(user = user)
+        return Response({
+            'token':token.key,
+            'user_id': user.pk,
+            'is_admin': user.is_admin
+        })
+class LogoutView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def post(self,request):
+        request.user.auth_token.delete()
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
+    
